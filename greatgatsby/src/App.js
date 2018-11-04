ba578f09a54d9cfda11a4ebdb4c7ec0de9324da9
Arunabh Das <arunabhdas@gmail.com>
2018-11-03 23:53:31 -0400

    Added call to firebase

--- greatgatsby/src/App.js
+++ greatgatsby/src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import {init as firebaseInit} from 'javascripts/firebase'
 import "tabler-react/dist/Tabler.css";
 import { Card, Button } from "tabler-react";
 class App extends Component {
+  constructor(props) {
+    super(props)
+    firebaseInit()
+  }
   render() {
     return (
       <div className="App">
